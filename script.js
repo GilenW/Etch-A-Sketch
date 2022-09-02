2f7141726da966dfdb2b4da907357b7d6ec4c0e0
GfromMacBook <gwuhou@ucsd.edu>
2022-09-02 16:26:47 -0700

    fix style

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const board = document.querySelector('.board');
 const size = document.querySelector('#gridSize');
+const color = document.querySelector('#colorPicker');
+const clean = document.querySelector('.clean');
+const buttons = document.querySelectorAll('button');
 var r = document.querySelector(':root');
-let grids;
+let isDrawing = false;
+let grids = document.querySelectorAll('.grid');
+let choseColor = color.value;
+document.body.onmousedown = () => (isDrawing = true)
+document.body.onmouseup = () => (isDrawing = false)
+creatDiv(16);
+
 function creatDiv(numberOfGrid){
     r.style.setProperty('--rowN', numberOfGrid);
     r.style.setProperty('--colN', numberOfGrid);
     for(let i = 1; i <= numberOfGrid*numberOfGrid; i++){
         let grid = document.createElement("div");
         grid.setAttribute("class", `grid ${i}`);
+        grid.addEventListener('mousedown', draw);
+        grid.addEventListener('mouseenter', draw);
 
         board.appendChild(grid);
     }
@@ -26,10 +37,34 @@ function changeSize(){
 
 }
 
-function setColor(){
-    this.style.backgroundColor = "red";
-    console.log(this);
+function draw(e){
+    if(e.type === 'mouseenter' && !isDrawing) return;
+    e.target.style.backgroundColor = `${choseColor}`;
 }
 
+
+function toggleFunction(){
+    switch (this.textContent){
+        case 'Clean':
+            grids = document.querySelectorAll('.grid');
+            grids.forEach(grid =>{
+                grid.style.backgroundColor = r.style.getPropertyValue('--canvaBackgroundColr');
+            })
+            break;
+        case 'Erase':
+            choseColor = r.style.getPropertyValue('--canvaBackgroundColr');
+            break;
+        case 'Draw':
+            console.log(this.textContent);
+            choseColor = `${color.value}`;
+        
+    }
+
+}
 size.addEventListener('input', changeSize);
-creatDiv(16);
+color.addEventListener('input', ()=> choseColor =`${color.value}` );
+buttons.forEach(button =>{
+    button.addEventListener('click', toggleFunction);
+})
+
+

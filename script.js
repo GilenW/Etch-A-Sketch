8c72b7eac149be44c7c544ecedcf503908db63db
GfromMacBook <gwuhou@ucsd.edu>
2022-09-02 11:16:25 -0700

    prev version

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const container = document.querySelector('.container');
-
+const board = document.querySelector('.board');
+const size = document.querySelector('#gridSize');
 function creatDiv(numberOfGrid){
     for(let i = numberOfGrid; i > 0; i--){
         let grid = document.createElement("div");
         grid.setAttribute("class", `grid ${i}`);
-        grid.textContent = `This is grid ${i}`;
+        // grid.textContent = `This is grid ${i}`;
 
-        console.log('before append child');
-        container.appendChild(grid);
-        console.log('after append child');
+        board.appendChild(grid);
+
 
 
     }
     return;
 }
 
-console.log('hello');
-creatDiv(3);
+function changeSize(){
+    
+    const widthHeightOfGrid = board.offsetWidth / this.value;
+    console.log('range value:' + this.value)
+;    console.log('board\'s width:' + board.offsetWidth);
+    console.log('single grid size:' + widthHeightOfGrid);
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid =>{
+        grid.remove();
+    })
+    creatDiv(this.value * this.value);
+    grids.forEach(grid =>{
+        grid.style.width = `${widthHeightOfGrid}px`;
+        grid.style.height = `${widthHeightOfGrid}px`;
+    })
+
+
+
+}
+size.addEventListener('change', changeSize);
+creatDiv((16*16));
